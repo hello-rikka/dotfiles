e1c7bacbbe73b581dd470cc636a4420f891741de
aira <meomeow98@ghotmail.com>
2024-09-24 20:23:44 +0700

    fix scrip changle wallpage

--- .config/script/selectswww.sh
+++ .config/script/selectswww.sh
@@ -4,13 +4,20 @@ dirImgMenu=~/.config/wallpapers/wallmenu
 handlerImage() {
   image=$(ls $imagePath | while read A; do echo -en "$A\x00icon\x1f$imagePath$A\n"; done | rofi -dmenu -theme $theme )
   link_img="${imagePath}${image}"
-
   imagecurrent=$(swww query)
   swwwsquery=$(echo $imagecurrent | awk -F'image: ' '{print $2}')
-    if [ "$swwwsquery" != "$link_img" ] && [ "$image" != "" ]; then
+  if [ $? -eq 0 ];then
+        date=$(date '+%Y-%m-%d-%H:%M:%S')
+        extension="${link_img##*.}"
+        echo $link_img
+        mv "$link_img" "$imagePath"wallpage"$date.$extension"
+        link_img="${imagePath}wallpage${date}.${extension}"
+  fi
+  if [ "$swwwsquery" != "$link_img" ]; then
       swww img $link_img --transition-fps 60 --transition-type wave --transition-duration 5 --transition-wave 10,20
       magick $link_img -resize 1000x600! $dirImgMenu
       dunstify -a "wallpaper" $image "done !" -u critical -I $link_img
-    fi
+  fi
+
 }
 handlerImage
