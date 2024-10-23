c8fb943b2e87492655cc07ab3954c411f6ca40df
aly <meomeow98@icloud.com>
2024-10-23 20:47:03 +0700

    new

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 DirIconLocal=$HOME/.icons
 DirThemeLocal=$HOME/.themes
 DirFontLocal=$HOME/.local/share/fonts
-DirConfigLocal=$HOME/.config/
+DirConfigLocal=$HOME/.config
 
 DirIcon=.icons/*
 DirTheme=.themes/*
@@ -62,7 +62,7 @@ fi
 
 
 
-package=(hyprland btop swaylock-effects hyprshot zsh sway network-manager-applet kitty blueman dunst rofi-lbonn-wayland-git jd-tool waybar swww cliphist polkit-kde-agent xdg-desktop-portal-hyprland brightnessctl pavucontrol pamixer nwg-look fcitx5-bamboo-git fcitx5-gtk fcitx5-configtool)
+package=(hyprland btop swaylock-effects hyprshot zsh sway network-manager-applet kitty blueman dunst rofi-lbonn-wayland-git jd-tool waybar swww cliphist polkit-kde-agent xdg-desktop-portal-hyprland brightnessctl pavucontrol pamixer nwg-look fcitx5-bamboo-git fcitx5-gtk fcitx5-configtool nemo atool )
 
 for variable in ${package[@]}
 do
@@ -79,24 +79,50 @@ do
             echo "$variable" >> ./log/logerrors.txt
         fi
     fi
-    
+
 done
-git clone https://github.com/meomeowth/wallpapers.git
-ls wallpapers
-if [ $? -eq 0 ]; then
-    echo "đã tải thành công"
-else
-    echo "tải không thành công"
+
+echo "Đã cài xong package"
+
+
+read -p "Bạn có muốn tải wallpaper không?(Y/N) : " bool
+echo "$bool"
+if [ $bool == "N" ]; then
+    mkdir $HOME/.config/wallpapers
+    if [ $? -eq 0 ]; then
+        echo "Tạo thư mục wallpapers thành công"
+    else
+        echo "Lỗi không tạo được thư mục wallpapers"
+        echo "$HOME/.config/wallpapers" >> ./log/logerrors.txt
+    fi
+    mkdir $HOME/.config/wallpapers/desktop
+    if [ $? -eq 0 ]; then
+        echo "Tạo thư mục desktop thành công"
+    else
+        echo "Lỗi không tạo được thư mục desktop"
+        echo "$HOME/.config/wallpapers/desktop" >> ./log/logerrors.txt
+    fi
+fi
+if [ $bool == "Y" ]; then
+    git clone https://github.com/meomeowth/wallpapers.git
+    ls wallpapers
+    if [ $? -eq 0 ]; then
+        echo "đã tải thành công"
+    else
+        echo "tải không thành công"
+    fi
+    
+    cp -r wallpapers $DirConfigLocal
+    if [ $? -eq 0 ]; then
+        echo "Đã config file wallpages"
+    else
+        echo "Config  không thành công"
+    fi
 fi
 
-cp -r wallpapers $DirConfigLocal
-if [ $? -eq 0 ]; then
-    echo "Đã config file wallpages"
-else
-    echo "Config  không thành công"
-fi
 
-# cp -r .config/* $HOME/.config/
+
+cp -r .config/* $HOME/.config/
 Cp "$DirConfig" "$DirConfigLocal"
 ls ~/.oh-my-zsh >/dev/null 2>&1
 if [ $? -eq 0 ]; then
@@ -104,11 +130,10 @@ if [ $? -eq 0 ]; then
 else
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
     echo "oh-my-zsh đã cài đặt"
+    
 fi
 
-# cp -r .config/* $HOME/.config/
 cp .zshrc ~/
-
 
 
 
